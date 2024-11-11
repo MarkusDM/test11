0f8b8f5344ee760409c0f7d97f0fdee1480c34f0
Dmitry Markov <mazarini2002@yandex.ru>
2024-11-11 14:38:20 +0500

    Add files via upload

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,65 +1,77 @@
 import gulp from 'gulp';
+import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import autoprefixer from 'gulp-autoprefixer';
 import imagemin from 'gulp-imagemin';
-import { deleteAsync } from 'del';
-import browserSync from 'browser-sync';
-import dartSass from 'sass';
 import webp from 'gulp-webp';
+import { deleteAsync } from 'del';
+import fileInclude from 'gulp-file-include'; // Подключение gulp-file-include
+import browserSync from 'browser-sync';
 
 const { src, dest, series, parallel, watch } = gulp;
-const sass = gulpSass(dartSass);
+
+const sassCompiler = gulpSass(dartSass);
 
 const paths = {
-  styles: {
-    src: 'app/scss/**/*.scss',
-    dest: 'dist/css'
-  },
-  images: {
-    src: 'app/images/**/*.{jpg,png}',
-    dest: 'dist/images'
-  },
-  html: {
-    src: 'app/*.html',
-    dest: 'dist'
-  }
+    styles: {
+        src: 'app/scss/**/*.scss',
+        dest: 'dist/css'
+    },
+    images: {
+        src: 'app/images/**/*.{jpg,png}',
+        dest: 'dist/images'
+    },
+    html: {
+        src: 'app/*.html',
+        dest: 'dist'
+    },
+    templates: {
+        src: 'app/template/**/*.html', // Путь к шаблонам
+        dest: 'dist/template'
+    }
 };
 
 async function clean() {
-  await deleteAsync(['dist']);
+    await deleteAsync(['dist']);
+}
+
+function processHtml() {
+    return src(paths.html.src)
+        .pipe(fileInclude({
+            prefix: '@',
+            basepath: 'app' // Базовая директория для инклюдов
+        }))
+        .pipe(dest(paths.html.dest))
+        .pipe(browserSync.stream());
 }
 
 function styles() {
-  return src(paths.styles.src)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({ cascade: false }))
-    .pipe(cleanCSS())
-    .pipe(dest(paths.styles.dest))
-    .pipe(browserSync.stream());
+    return src(paths.styles.src)
+        .pipe(sassCompiler().on('error', sassCompiler.logError))
+        .pipe(autoprefixer({ cascade: false }))
+        .pipe(cleanCSS())
+        .pipe(dest(paths.styles.dest))
+        .pipe(browserSync.stream());
 }
 
 function convertToWebp() {
-  return src(paths.images.src)
-    .pipe(imagemin())
-    .pipe(webp())
-    .pipe(dest(paths.images.dest));
-}
-
-function html() {
-  return src(paths.html.src)
-    .pipe(dest(paths.html.dest))
-    .pipe(browserSync.stream());
+    return src(paths.images.src)
+        .pipe(imagemin())
+        .pipe(webp())
+        .pipe(dest(paths.images.dest));
 }
 
 function serve() {
-  browserSync.init({
-    server: './dist'
-  });
+    browserSync.init({
+        server: './dist'
+    });
 
-  watch(paths.styles.src, styles);
-  watch(paths.images.src, convertToWebp);
-  watch(paths.html.src, html).on('change', browserSync.reload);
+    watch(paths.styles.src, styles);
+    watch(paths.images.src, convertToWebp);
+    watch(paths.html.src, processHtml);
+    watch(paths.templates.src, processHtml); // Следить за шаблонами
 }
 
-export default series(clean, parallel(styles, convertToWebp, html), serve);
+export default series(clean, parallel(styles, convertToWebp, processHtml), serve);
+
